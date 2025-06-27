579a606e1019f079a46384a0d8255354dbdfe854
Mi-kode <folan.t@hotmail.com>
2025-06-27 18:44:28 +0200

    Update

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_26_104432) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_27_162439) do
   create_table "active_storage_caches", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
